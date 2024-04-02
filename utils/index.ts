45aff8aca12629e5576dc40c237330098fda88e5
sleekcharly <c_ukasoanya@yahoo.com>
2024-04-02 02:42:32 +0100

    project halted due to API key subscription

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types';
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': '3e3c3d601emsh563dfd96e5e6c43p13f1abjsn444e146e3a5f',
@@ -55,4 +57,22 @@ export const deleteSearchParams = (type: string) => {
   }?${newSearchParams.toString()}`;
 
   return newPathname;
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    'customer',
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '',
+  );
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
